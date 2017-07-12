513d103d544cd627c686ca5ef93d3d45f72ddd8f
Hari Narayan Das <hndas16@gmail.com>
2017-07-12 08:26:04 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    HN DAS
                 </div>
 
                 <div class="links">
