0f4d2554870c8863affaecc2f63515ba7e9ab05d
ruhi16 <hndas16@gmail.com>
2017-07-12 14:27:53 +0530

    fc5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    H N DAS
+                    H N DAS Test...
                 </div>
 
                 <div class="links">
